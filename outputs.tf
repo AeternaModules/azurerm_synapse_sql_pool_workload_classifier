e80281774eeacbae04288802eeb49ee94d6592cb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "synapse_sql_pool_workload_classifiers" {
-  description = "All synapse_sql_pool_workload_classifier resources"
-  value       = azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers
-}
 output "synapse_sql_pool_workload_classifiers_context" {
-  description = "List of context values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.context]
+  description = "Map of context values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.context }
 }
 output "synapse_sql_pool_workload_classifiers_end_time" {
-  description = "List of end_time values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.end_time]
+  description = "Map of end_time values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.end_time }
 }
 output "synapse_sql_pool_workload_classifiers_importance" {
-  description = "List of importance values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.importance]
+  description = "Map of importance values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.importance }
 }
 output "synapse_sql_pool_workload_classifiers_label" {
-  description = "List of label values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.label]
+  description = "Map of label values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.label }
 }
 output "synapse_sql_pool_workload_classifiers_member_name" {
-  description = "List of member_name values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.member_name]
+  description = "Map of member_name values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.member_name }
 }
 output "synapse_sql_pool_workload_classifiers_name" {
-  description = "List of name values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.name]
+  description = "Map of name values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.name }
 }
 output "synapse_sql_pool_workload_classifiers_start_time" {
-  description = "List of start_time values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.start_time]
+  description = "Map of start_time values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.start_time }
 }
 output "synapse_sql_pool_workload_classifiers_workload_group_id" {
-  description = "List of workload_group_id values across all synapse_sql_pool_workload_classifiers"
-  value       = [for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : v.workload_group_id]
+  description = "Map of workload_group_id values across all synapse_sql_pool_workload_classifiers, keyed the same as var.synapse_sql_pool_workload_classifiers"
+  value       = { for k, v in azurerm_synapse_sql_pool_workload_classifier.synapse_sql_pool_workload_classifiers : k => v.workload_group_id }
 }
 
